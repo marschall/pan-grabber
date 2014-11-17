29065d4f6e90356475c2504ac568c32bc7bd0144
Philippe Marschall <philippe.marschall@gmail.com>
2014-11-17 07:59:33 +0100

    bump

---
+++ src/main/c/pangrab.c
@@ -0,0 +1,82 @@
+#include <jni.h>
+#include <jvmti.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+/* https://docs.oracle.com/javase/7/docs/platform/jvmti/jvmti.html#IterateThroughHeap */
+/* https://docs.oracle.com/javase/7/docs/platform/jvmti/jvmti.html#onload */
+
+bool checkLuhn(const jchar* value, jint value_length) {
+    int sum = 0;
+    if (value_length != 16) {
+        return false;
+    }
+    /* luhn iterates the other way but we know the length is 16 */
+    for (int i = 0; i < 16 ; ++i) {
+        jchar digit = value[i];
+        int value;
+        if (value < '0' | value > '9') {
+            return false;
+        }
+        value = digit - '0';
+        /* we know length is 16 */
+        /* TODO optimize */
+        if (i % 2 == 0) {
+            value = value * 2;
+        }
+        if (value > 9) {
+            value = value - 9;
+        }
+        sum = sum + value;
+    }
+    return sum % 10 == 0;
+}
+
+jint JNICALL StringPrimitiveValueCallback(jlong class_tag,  jlong size, 
+                                          jlong* tag_ptr,
+                                          const jchar* value,
+                                          jint value_length,
+                                          void* user_data) {
+    if (checkLuhn(value, value_length)) {
+        printf("found pan\n");
+    }
+    return JVMTI_VISIT_OBJECTS;
+}
+
+JNIEXPORT jint JNICALL Agent_OnLoad(JavaVM *vm, char *options, void *reserved) {
+    fprintf(stderr, "Agent_OnLoad not supported, only Agent_OnAttach\n");
+    return -1;
+}
+
+JNIEXPORT jint JNICALL Agent_OnAttach(JavaVM* vm, char *options, void *reserved) {
+    jvmtiEnv* jvmti;
+    JNIEnv* env;
+    jvmtiHeapCallbacks callbacks;
+    jclass stringClass;
+    
+    if ((*vm)->GetEnv(vm, (void**)&jvmti, JVMTI_VERSION_1_0) != JNI_OK) {
+        fprintf(stderr, "GetEnv failed\n");
+        return -1;
+    }
+    
+    (*vm)->GetEnv( (void**)&env, JNI_VERSION_1_2 );
+    
+    stringClass = (*env)->FindClass(env, "java/lang/String");
+    if (stringClass == NULL) {
+        // TODO check for exception
+        fprintf(stderr, "Can't find String class\n");
+        return -1;
+    }
+    
+    memset(&callbacks, 0, sizeof(callbacks));
+    callbacks.string_primitive_value_callback = StringPrimitiveValueCallback;
+    
+    if((*jvmti)->IterateThroughHeap(jvmti, 0, stringClass, &callbacks, (void*) NULL) != JVMTI_ERROR_NONE) {
+        fprintf(stderr,"IterateThroughHeap failed.\n");
+        return -1;
+    }
+    
+    return 0;
+}
+
