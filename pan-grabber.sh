434a9ed99a650dc59b2790190f0a605cedce03ed
Philippe Marschall <philippe.marschall@gmail.com>
2014-11-18 16:25:24 +0100

    add shell scripts to start application

---
+++ pan-grabber.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PAN_GRABBER_JAR_FILE=`find target/ -name "pan-grabber*.jar" | grep -v javadoc | grep -v sources`
+
+AGENT_PATH=
+AGENT_PATH=`find target/ -name "*.so"`
+if [ not $AGENT_PATH ]; then
+  AGENT_PATH=`find target/ -name "*.jnilib"`
+fi
+
+echo $AGENT_PATH
+
+$JAVA_HOME/bin/java -cp $JAVA_HOME/lib/tools.jar:$PAN_GRABBER_JAR_FILE com.github.marschall.pangrabber.Attacher -a $AGENT_PATH
---
+++ sample-application.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+$JAVA_HOME/bin/java -cp target/test-classes/ com.github.marschall.pangrabber.SampleApplication
